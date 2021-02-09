4f3083e8ce61bf9d20595633363459172a3322a3
Lucie Hulin <luciehulin@macbook-pro-de-lucie-2.home>
2021-02-09 10:02:18 +0100

    Initial commit

---
+++ src/main/java/org/polytech/shipme/ShipmeApplication.java
@@ -0,0 +1,13 @@
+package org.polytech.shipme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/polytech/shipme/ShipmeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.polytech.shipme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
